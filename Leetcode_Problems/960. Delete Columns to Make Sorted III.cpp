3cc9724370e3fe7b86091f3df97b5c8654d3b277
SUBHAJIT DAS <subhajit.das.1768@gmail.com>
2025-12-24 23:18:53 +0530

    Added LeetCode solutions (Reverse Integer, Pow(x,n), DP problems)

---
+++ Leetcode_Problems/960. Delete Columns to Make Sorted III.cpp
@@ -0,0 +1,67 @@
+// You are given an array of n strings strs, all of the same length.
+
+// We may choose any deletion indices, and we delete all the characters in those indices for each string.
+
+// For example, if we have strs = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions is ["bef", "vyz"].
+
+// Suppose we chose a set of deletion indices answer such that after deletions, the final array has every string (row) in lexicographic order. (i.e., (strs[0][0] <= strs[0][1] <= ... <= strs[0][strs[0].length - 1]), and (strs[1][0] <= strs[1][1] <= ... <= strs[1][strs[1].length - 1]), and so on). Return the minimum possible value of answer.length.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["babca","bbazb"]
+// Output: 3
+// Explanation: After deleting columns 0, 1, and 4, the final array is strs = ["bc", "az"].
+// Both these rows are individually in lexicographic order (ie. strs[0][0] <= strs[0][1] and strs[1][0] <= strs[1][1]).
+// Note that strs[0] > strs[1] - the array strs is not necessarily in lexicographic order.
+// Example 2:
+
+// Input: strs = ["edcba"]
+// Output: 4
+// Explanation: If we delete less than 4 columns, the only row will not be lexicographically sorted.
+// Example 3:
+
+// Input: strs = ["ghi","def","abc"]
+// Output: 0
+// Explanation: All rows are already lexicographically sorted.
+ 
+
+// Constraints:
+
+// n == strs.length
+// 1 <= n <= 100
+// 1 <= strs[i].length <= 100
+// strs[i] consists of lowercase English letters.
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+
+        vector<int> dp(m, 1);
+
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < j; i++) {
+                bool ok = true;
+                for (int r = 0; r < n; r++) {
+                    if (strs[r][i] > strs[r][j]) {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        int maxKeep = 0;
+        for (int x : dp) maxKeep = max(maxKeep, x);
+
+        return m - maxKeep;
+    }
+};
+
+ 
