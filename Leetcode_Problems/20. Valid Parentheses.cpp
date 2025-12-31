c4b6d4a89caca23f99527bd556c5a725d4b3853a
SUBHAJIT DAS <subhajit.das.1768@gmail.com>
2025-12-31 23:31:24 +0530

    Add files via upload

---
+++ Leetcode_Problems/20. Valid Parentheses.cpp
@@ -0,0 +1,74 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+// Example 5:
+
+// Input: s = "([)]"
+
+// Output: false
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            // Closing brackets
+            else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
